8413e84f0b063baf6def9479ff5ceee68ca67e03
Furkan Aktaş <furkakts_4126@hotmail.com>
2024-07-05 12:13:33 +0300

    Initial Commit

---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Furkan on 5.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
