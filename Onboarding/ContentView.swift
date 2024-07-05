a6a1c5241e126ab7466cdc102b86c509c4c311e8
Furkan Aktaş <furkakts_4126@hotmail.com>
2024-07-05 13:51:01 +0300

    [Feature] Homepage View Added.

--- Onboarding/ContentView.swift
+++ Onboarding/ContentView.swift
@@ -1,22 +1,64 @@
-//
-//  ContentView.swift
-//  Onboarding
-//
-//  Created by Furkan on 5.07.2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("isShown") private var isShown = true
+    @State private var pageNumber = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color("MainColor").ignoresSafeArea()
+            
+            if isShown{
+                
+           
+            } else {
+                homepageView
+            }
+        }
+        
+    }
+}
+
+extension ContentView {
+    var homepageView : some View{
+        VStack(spacing:50){
+            Image("InMind")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .cornerRadius(10)
+            
+            Text("Finally reached the end of Onboarding journey.If you did get all the points of my application, go on and enjoy it.If you have still doubts about the app's capabilities,get back to Onboarding journey!")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .foregroundStyle(Color("MainColor"))
+                .lineSpacing(4)
+                .kerning(0.5)
+                .multilineTextAlignment(.center)
+            
+            Button{
+                withAnimation(.easeOut){
+                    isShown = false
+                    pageNumber = 1
+                }
+            } label: {
+                Text("Back to Journey!")
+            }
+            .padding()
+            .font(.headline)
+            .foregroundStyle(Color("SecondaryColor"))
+            .background(Color("MainColor"), in:RoundedRectangle(cornerRadius: 10))
+            .shadow(color:Color("MainColor"), radius: 2)
         }
         .padding()
+        .frame(width:UIScreen.main.bounds.size.width - 20, height:400)
+        .background{
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color("SecondaryColor"))
+        }
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
