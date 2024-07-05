b2d4b27420edf66eebc6c63e7fc703db7da3fa9c
Furkan Aktaş <furkakts_4126@hotmail.com>
2024-07-06 01:19:17 +0300

    [Refactor] Comment and Correction
    Comments added.Clean code rules applied.And title for homepage added.

--- Onboarding/ContentView.swift
+++ Onboarding/ContentView.swift
@@ -1,6 +1,10 @@
+// Made,Coded,Created,Designed
+// by Furkan "Junior" Aktas,  on 05 July 2024
+
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Main View
     @AppStorage("isShown") private var onBoardingIsShown = false
     @State private var pageNumberInOnboardingView = 1
     
@@ -10,15 +14,15 @@ struct ContentView: View {
     }
     
     private let mockData:[String] = MockDataModel.mockTextData
-    private var messageForEachOnboardingPage:String{
+    private var messageForEachOnboardingPage:String {
         mockData[pageNumberInOnboardingView-1]
     }
     var body: some View {
         ZStack(alignment:.bottom) {
             Color("MainColor").ignoresSafeArea()
             
-            if !onBoardingIsShown{
-                switch pageNumberInOnboardingView {  // Visit over pages of onboarding
+            if !onBoardingIsShown {
+                switch pageNumberInOnboardingView {  // Visit over onboarding pages
                    case 1:
                     OnboardingView(pageNumber: $pageNumberInOnboardingView, isShown: $onBoardingIsShown,
                                    title: titleForEachOnboardingPage, message: messageForEachOnboardingPage, numberOfPages: mockData.count)
@@ -32,28 +36,29 @@ struct ContentView: View {
                     OnboardingView(pageNumber: $pageNumberInOnboardingView, isShown: $onBoardingIsShown,
                                    title: titleForEachOnboardingPage, message: messageForEachOnboardingPage, numberOfPages: mockData.count)
                    default:
-                      Text("Empty View")
+                      Text("Empty View")  // Empty case
                 }
             } else {
                 homepageView }
         }  // End ZStack
     }
 }
-
+  // MARK: - Onboarding
 struct OnboardingView:View {
     @Binding fileprivate var pageNumber:Int
     @Binding fileprivate var isShown:Bool
+    
     fileprivate var title:String
     fileprivate var message:String
     fileprivate let numberOfPages:Int
     private let appName = "InMind"
     
     var body:some View {
-        VStack(spacing:10) {
+        VStack(spacing:10){
             Image(appName)
                 .resizable()
                 .scaledToFit()
-                .frame(width: 100, height: 100)
+                .frame(width:100, height:100)
                 .cornerRadius(10)
             Text(appName)
                 .font(.title2)
@@ -74,61 +79,56 @@ struct OnboardingView:View {
                 .multilineTextAlignment(.center)
             Spacer()
             HStack(spacing:30){
-                Group{
+                Group{   // This part decides when and how the buttons will appear according to pageNumber.
                     if pageNumber != 1 {
-                        Button("Back"){ withAnimation{pageNumber-=1}}
+                        Button("Back"){ withAnimation(.easeOut){pageNumber-=1} }
                     }
                     if pageNumber != 4 {
-                        Button("Next"){ withAnimation{pageNumber+=1}}
+                        Button("Next"){ withAnimation(.easeOut){pageNumber+=1} }
                     } else {
-                        Button("Finish") { isShown = true }
+                        Button("Finish"){ isShown = true }
                     }
                 }
-                .padding(.horizontal, 50)
-                .padding(.vertical, 10)
+                .padding(.horizontal,50)
+                .padding(.vertical,10)
                 .font(.headline)
                 .foregroundStyle(Color("SecondaryColor"))
-                .background(Color("MainColor"), in:RoundedRectangle(cornerRadius: 10))
-                .shadow(color:Color("MainColor"), radius: 1)
+                .background(Color("MainColor"), in:RoundedRectangle(cornerRadius:10))
+                .shadow(color:Color("MainColor"), radius:1)
             }
             Spacer()
-            HStack {
-                ForEach(1..<numberOfPages+1){number in
-                    Rectangle()
+            HStack{
+                ForEach(1..<numberOfPages+1){number in    // This part sets little circles for showing pages at the bottom of view.
+                    Circle()
                         .fill(Color("MainColor"))
-                        .frame(width: pageNumber==number ? 15 : 10, height:10)
-                        .cornerRadius(5)
+                        .frame(width:10, height:10)
                         .opacity(pageNumber==number ? 1.0 : 0.3)
-                    }
+                }
             }
         }
         .padding()
         .frame(width:UIScreen.main.bounds.size.width - 20, height:500)
-        .background{
-            RoundedRectangle(cornerRadius: 15)
-                .fill(Color("SecondaryColor"))
-        }
-        .shadow(color:Color("SecondaryColor"),radius: 3)
+        .background(Color("SecondaryColor"), in:RoundedRectangle(cornerRadius:15))
+        .shadow(color:Color("SecondaryColor"),radius:3)
         .transition(.move(edge: .trailing))
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
+ // MARK: - Homepage View
 extension ContentView {
-    var homepageView : some View{
+    var homepageView: some View {
         VStack(spacing:10){
             Image("InMind")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 100, height: 100)
+                .frame(width:100, height:100)
                 .cornerRadius(10)
             Text("InMind")
                 .font(.title2)
+                .bold()
+                .foregroundStyle(Color("MainColor"))
+            Spacer()
+            Text("Happy Onboarding!")
+                .font(.title3)
                 .bold()
                 .foregroundStyle(Color("MainColor"))
             Spacer()
@@ -140,24 +140,27 @@ extension ContentView {
                 .kerning(0.5)
                 .multilineTextAlignment(.center)
             Spacer()
-            Button("Back to Journey"){
+            Button("Back to Journey"){ // Directs back to beginning.
                withAnimation{
                    onBoardingIsShown = false
                    pageNumberInOnboardingView = 1
                }
             }
-            .padding(.horizontal, 12)
-            .padding(.vertical, 12)
+            .padding(.horizontal,12)
+            .padding(.vertical,12)
             .font(.headline)
             .foregroundStyle(Color("SecondaryColor"))
-            .background(Color("MainColor"), in:RoundedRectangle(cornerRadius: 10))
+            .background(Color("MainColor"), in:RoundedRectangle(cornerRadius:10))
         }
         .padding()
         .frame(width:UIScreen.main.bounds.size.width - 20, height:500)
-        .background{
-            RoundedRectangle(cornerRadius: 15)
-                .fill(Color("SecondaryColor"))
-        }
-        .shadow(color:Color("SecondaryColor"),radius: 3)
+        .background(Color("SecondaryColor"), in:RoundedRectangle(cornerRadius: 15))
+        .shadow(color:Color("SecondaryColor"),radius:3)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
